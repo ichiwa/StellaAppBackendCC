ad530b695b9f54db3e2fb6618d5bbc1e7b9ee96f
Ichiwa <thankyou46585960@gmail.com>
2015-07-27 21:24:39 +0900

    added user_add api

---
+++ cloud/api/user_add.js
@@ -0,0 +1,46 @@
+var UserInfo = Parse.Object.extend("UserInfo");
+var UserHouseWork = Parse.Object.extend("UserHouseWork");
+
+Parse.Cloud.define("user_add", function(request, response) {
+    createUserId()
+        .then(
+            function(userId) {
+                var user = new UserInfo();
+                user.set("userId", userId);
+                return user.save();
+            }
+        )
+        .then(
+            function(userInfo) {
+                response.success(userInfo);
+            },
+            function() {
+                response.error("error");
+            }
+        )
+})
+
+function createUserId(promise) {
+    var promise = promise || new Parse.Promise();
+    // よくあるユーザID 0 ~ 999,999,999 
+    var userId = Math.floor(Math.random() * 999999999);
+    var query = new Parse.Query("UserInfo");
+    query.equalTo("userId", userId);
+    query
+        .first()
+        .then(
+            function(userInfo) {
+                if (userInfo) {
+                    // 既に存在するuserIdなら再帰する
+                    return createUserId(promise);
+                }
+                else {
+                    promise.resolve(userId);
+                }
+            },
+            function(error) {
+                promise.error();
+            }
+        )
+    return promise;
+}
